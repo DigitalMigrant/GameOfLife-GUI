b72bba4795eaf546af4f9906b94cf7a0e01bbd3d
DigitalMigrant <roselia.delacruz@gmail.com>
2023-12-14 12:57:26 -0800

    Initial Commit

---
+++ Assignment_1_ICT711_RDelaCruz/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace Assignment_1_ICT711
+{
+    partial class Game_of_life_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.file_operation_group = new System.Windows.Forms.GroupBox();
+            this.reset_all_button = new System.Windows.Forms.Button();
+            this.save_Grid_button = new System.Windows.Forms.Button();
+            this.load_grid_button = new System.Windows.Forms.Button();
+            this.single_step_Group = new System.Windows.Forms.GroupBox();
+            this.generation_Label = new System.Windows.Forms.Label();
+            this.step_textBox = new System.Windows.Forms.TextBox();
+            this.next_gen_button = new System.Windows.Forms.Button();
+            this.game_control_group = new System.Windows.Forms.GroupBox();
+            this.exit_button = new System.Windows.Forms.Button();
+            this.label_grid_groupBox = new System.Windows.Forms.GroupBox();
+            this.file_operation_group.SuspendLayout();
+            this.single_step_Group.SuspendLayout();
+            this.game_control_group.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // file_operation_group
+            // 
+            this.file_operation_group.Controls.Add(this.reset_all_button);
+            this.file_operation_group.Controls.Add(this.save_Grid_button);
+            this.file_operation_group.Controls.Add(this.load_grid_button);
+            this.file_operation_group.Location = new System.Drawing.Point(23, 50);
+            this.file_operation_group.Name = "file_operation_group";
+            this.file_operation_group.Size = new System.Drawing.Size(295, 191);
+            this.file_operation_group.TabIndex = 1;
+            this.file_operation_group.TabStop = false;
+            this.file_operation_group.Text = "File Operations";
+            // 
+            // reset_all_button
+            // 
+            this.reset_all_button.Location = new System.Drawing.Point(19, 134);
+            this.reset_all_button.Name = "reset_all_button";
+            this.reset_all_button.Size = new System.Drawing.Size(112, 44);
+            this.reset_all_button.TabIndex = 3;
+            this.reset_all_button.Text = "&Reset All Cells";
+            this.reset_all_button.UseVisualStyleBackColor = true;
+            this.reset_all_button.Click += new System.EventHandler(this.Reset_All_button_Click);
+            // 
+            // save_Grid_button
+            // 
+            this.save_Grid_button.Location = new System.Drawing.Point(19, 84);
+            this.save_Grid_button.Name = "save_Grid_button";
+            this.save_Grid_button.Size = new System.Drawing.Size(112, 44);
+            this.save_Grid_button.TabIndex = 2;
+            this.save_Grid_button.Text = "&Save Grid";
+            this.save_Grid_button.UseVisualStyleBackColor = true;
+            this.save_Grid_button.Click += new System.EventHandler(this.Save_Grid_button_Click);
+            // 
+            // load_grid_button
+            // 
+            this.load_grid_button.Location = new System.Drawing.Point(19, 34);
+            this.load_grid_button.Name = "load_grid_button";
+            this.load_grid_button.Size = new System.Drawing.Size(112, 44);
+            this.load_grid_button.TabIndex = 1;
+            this.load_grid_button.Tag = "";
+            this.load_grid_button.Text = "&Load Grid";
+            this.load_grid_button.UseVisualStyleBackColor = true;
+            this.load_grid_button.Click += new System.EventHandler(this.Load_Grid_button_Click);
+            // 
+            // single_step_Group
+            // 
+            this.single_step_Group.Controls.Add(this.generation_Label);
+            this.single_step_Group.Controls.Add(this.step_textBox);
+            this.single_step_Group.Controls.Add(this.next_gen_button);
+            this.single_step_Group.Location = new System.Drawing.Point(23, 272);
+            this.single_step_Group.Name = "single_step_Group";
+            this.single_step_Group.Size = new System.Drawing.Size(294, 108);
+            this.single_step_Group.TabIndex = 3;
+            this.single_step_Group.TabStop = false;
+            this.single_step_Group.Text = "Single Step";
+            // 
+            // generation_Label
+            // 
+            this.generation_Label.AutoSize = true;
+            this.generation_Label.Location = new System.Drawing.Point(178, 52);
+            this.generation_Label.Name = "generation_Label";
+            this.generation_Label.Size = new System.Drawing.Size(100, 20);
+            this.generation_Label.TabIndex = 2;
+            this.generation_Label.Text = "Generations";
+            // 
+            // step_textBox
+            // 
+            this.step_textBox.BackColor = System.Drawing.SystemColors.Window;
+            this.step_textBox.Location = new System.Drawing.Point(136, 49);
+            this.step_textBox.Name = "step_textBox";
+            this.step_textBox.Size = new System.Drawing.Size(36, 27);
+            this.step_textBox.TabIndex = 1;
+            // 
+            // next_gen_button
+            // 
+            this.next_gen_button.Location = new System.Drawing.Point(19, 41);
+            this.next_gen_button.Name = "next_gen_button";
+            this.next_gen_button.Size = new System.Drawing.Size(111, 38);
+            this.next_gen_button.TabIndex = 0;
+            this.next_gen_button.Text = "&Next";
+            this.next_gen_button.UseVisualStyleBackColor = true;
+            this.next_gen_button.Click += new System.EventHandler(this.Next_gen_button_Click);
+            // 
+            // game_control_group
+            // 
+            this.game_control_group.BackColor = System.Drawing.Color.DarkGray;
+            this.game_control_group.Controls.Add(this.exit_button);
+            this.game_control_group.Controls.Add(this.file_operation_group);
+            this.game_control_group.Controls.Add(this.single_step_Group);
+            this.game_control_group.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.game_control_group.Location = new System.Drawing.Point(776, 12);
+            this.game_control_group.Name = "game_control_group";
+            this.game_control_group.Size = new System.Drawing.Size(332, 699);
+            this.game_control_group.TabIndex = 4;
+            this.game_control_group.TabStop = false;
+            this.game_control_group.Text = "Game Controls";
+            // 
+            // exit_button
+            // 
+            this.exit_button.Location = new System.Drawing.Point(194, 629);
+            this.exit_button.Name = "exit_button";
+            this.exit_button.Size = new System.Drawing.Size(124, 42);
+            this.exit_button.TabIndex = 4;
+            this.exit_button.Text = "E&xit";
+            this.exit_button.UseVisualStyleBackColor = true;
+            this.exit_button.Click += new System.EventHandler(this.Exit_button_Click);
+            // 
+            // label_grid_groupBox
+            // 
+            this.label_grid_groupBox.Location = new System.Drawing.Point(0, 12);
+            this.label_grid_groupBox.Name = "label_grid_groupBox";
+            this.label_grid_groupBox.Size = new System.Drawing.Size(771, 698);
+            this.label_grid_groupBox.TabIndex = 5;
+            this.label_grid_groupBox.TabStop = false;
+            this.label_grid_groupBox.Text = "Grid";
+            // 
+            // Game_of_life_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1120, 723);
+            this.Controls.Add(this.label_grid_groupBox);
+            this.Controls.Add(this.game_control_group);
+            this.Name = "Game_of_life_Form";
+            this.Text = "Game of LIfe";
+            this.Load += new System.EventHandler(this.Game_of_life_Form_Load);
+            this.file_operation_group.ResumeLayout(false);
+            this.single_step_Group.ResumeLayout(false);
+            this.single_step_Group.PerformLayout();
+            this.game_control_group.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox file_operation_group;
+        private System.Windows.Forms.Button load_grid_button;
+        private System.Windows.Forms.Button reset_all_button;
+        private System.Windows.Forms.Button save_Grid_button;
+        private System.Windows.Forms.GroupBox single_step_Group;
+        private System.Windows.Forms.TextBox step_textBox;
+        private System.Windows.Forms.Button next_gen_button;
+        private System.Windows.Forms.Label generation_Label;
+        private System.Windows.Forms.GroupBox game_control_group;
+        private System.Windows.Forms.Button exit_button;
+        private System.Windows.Forms.GroupBox label_grid_groupBox;
+    }
+}
+
---
+++ Assignment_1_ICT711_RDelaCruz/Form1.cs
@@ -0,0 +1,346 @@
+﻿
+//
+//  Author:  Roselia Dela Cruz
+//
+//  Purpose:  Assignment 1  ICT 711 - Computer Programming Level 2
+//                                
+//  Date Created: November 30, 2022
+//
+//  Description:  A program written to demonstrate John Conway's Game of Life.
+
+
+
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace Assignment_1_ICT711
+{
+    public partial class Game_of_life_Form : Form
+    {
+        const int number_of_Rows = 30;
+        const int number_of_Cols = 30;
+        Status[,] currentGen = new Status[number_of_Rows, number_of_Cols]; //enum type array to hold current generation
+        Status[,] nextGen = new Status[number_of_Rows, number_of_Cols];//enum type array to hold next generation
+        Label[,] box = new Label[number_of_Rows, number_of_Cols]; //label array that serves as the grid
+     
+
+        public Game_of_life_Form()
+        {
+            InitializeComponent();
+        }
+        // Game_of_life_Form_Load()
+        //
+        // This method will load the form and draw a grid of label boxes according to predefined rows and columns.
+        //
+        private void Game_of_life_Form_Load(object sender, EventArgs e)
+        {
+          
+            for (int i = 0; i < number_of_Rows; i++)
+            {
+                for (int j = 0; j < number_of_Cols; j++)
+                {
+                    box[i, j] = new Label();
+
+                    box[i, j].Name = "grid_" + "x" + i.ToString() + "y" + j.ToString();
+                    box[i, j].Location = new Point(18, 18 + (i * 18));
+                    box[i, j].Size = new Size(18, 18);
+                    box[i, j].BackColor = Color.White;
+                    box[i, j].BorderStyle = BorderStyle.FixedSingle;
+                  
+                    box[i, j].Click += AnyBox_Click;
+                                       
+                    if (j > 0)
+                    {
+                        box[i, j].Left = box[i, j - 1].Right;
+                    }
+
+                    this.label_grid_groupBox.Controls.Add(box[i, j]);
+                }
+            }
+            label_grid_groupBox.Enabled = true;
+        }
+        // AnyBox_Click()
+        //
+        // This will enable the user to click on any box to change its color to either white or magenta.
+        //
+        private void AnyBox_Click(object sender, EventArgs e)
+        {
+            Label b = sender as Label;
+            if (b.BackColor == Color.White)
+            {
+                b.BackColor = Color.Magenta;
+            }
+            else
+            {
+                b.BackColor = Color.White;
+            }
+
+        }
+
+        // Status
+        //
+        // An enumeration type declared to represent thw two status of every cell in the grid array
+        //
+        public enum Status
+        {
+            Dead,
+            Alive,
+        }
+        // Load_Grid_button_Click()
+        //
+        // This will enable the user to load existing .GOL files. 
+        //
+        private void Load_Grid_button_Click(object sender, EventArgs e)
+        {
+
+            string current_path;//holds the path of currently opened file ****
+            String[] lines; //holds data values per line from text/.gol file ***
+            String[] coordinate_per_line; //holds coordinate from a line after removing delimeters ****
+
+            //reset the grid before loading new one
+            Reset_Grid();
+
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (DialogResult.OK == dialog.ShowDialog())
+            {
+                current_path = dialog.FileName;
+              
+                using (StreamReader r = File.OpenText(current_path))
+                {
+                    string text = r.ReadToEnd();
+                    lines = text.Split('\n'); //split the values found per line
+
+                    //for every line, check if empty, split coordinate values per delimiter
+                    //before setting up the grid
+                   
+                    foreach (string line in lines)
+                    {
+                        if (line.Trim() == "")
+                            continue;
+                        else
+                        {
+                            coordinate_per_line = line.Split(new char[] { ',', ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                            int x_coordinate = Convert.ToInt32(coordinate_per_line[0]);
+                            int y_coordinate = Convert.ToInt32(coordinate_per_line[1]);
+                            box[x_coordinate, y_coordinate].BackColor = Color.Magenta;
+                            currentGen[x_coordinate, y_coordinate] = Status.Alive;
+                        }
+                    }
+                }
+            }
+            
+            label_grid_groupBox.Enabled = false; //disable grid to avoid user click;
+
+        }
+        // Save_Grid_button_Click()
+        //
+        // This will call the Current_Grid() to determine the last state and will write the coordinates of every alive cell 
+        // found to a .GOL file. the user must enter the new file name or overwrite an existing file.
+        //
+        private void Save_Grid_button_Click(object sender, EventArgs e)
+        {
+            Current_Grid();
+
+            var user_dialog_answer = MessageBox.Show("Save the grid as .gol file", "Save Grid File", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+            if (user_dialog_answer == DialogResult.Cancel)
+            {
+                MessageBox.Show("Grid not saved.", "Save Grid File", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                //if ok, save to new file
+                SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+                saveFileDialog1.Filter = "GOL File|*.gol";
+                saveFileDialog1.Title = "Save as GOL File";
+                saveFileDialog1.ShowDialog();
+
+                // If the file name is not an empty string open it for saving.
+                if (saveFileDialog1.FileName != "")
+                {
+                    System.IO.StreamWriter SaveFile = new System.IO.StreamWriter(saveFileDialog1.FileName);
+                   
+                    for (int row = 0; row < number_of_Rows; row++)
+                    {
+                        for (int col = 0; col < number_of_Cols; col++)
+                        {
+                            if (currentGen[row, col] == Status.Alive) 
+                            {
+                                SaveFile.WriteLine(row + " " + col);//must write by pair
+                            }
+                        }
+                    }
+                    SaveFile.Close();
+                    MessageBox.Show("Grid saved!", "Save Grid File", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        // Next_gen_button_Click()
+        //
+        // This will call the Current_Grid(), NextGeneration(), and Print_Grid() methods
+        // to determine the current status and will calculate/print the next generation based
+        // on the number of steps from user input
+        //
+        private void Next_gen_button_Click(object sender, EventArgs e)
+        {
+            Current_Grid();
+            try
+            {
+                int num_of_steps = Convert.ToInt32(step_textBox.Text);
+
+                //call nextGen here based on number of steps
+                for (int step_counter = 1; step_counter <= num_of_steps; step_counter++)
+                {
+                    nextGen = NextGeneration(currentGen);
+                    currentGen = nextGen;
+                    // print the new gen
+                    Print_Grid(currentGen);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Please enter an integer only.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // NextGeneration() - Determine the next generation
+        //
+        // This will loop through the grid and find the alive neighbors 
+        //
+        private static Status[,] NextGeneration(Status[,] currentGrid)
+        {
+            var nextGeneration = new Status[number_of_Rows, number_of_Cols];
+
+            // Loop through every cell 
+            for (var row = 1; row < number_of_Rows - 1; row++)
+                for (var column = 1; column < number_of_Cols - 1; column++)
+                {
+                    // find the alive neighbors
+                    var aliveNeighbors = 0;
+                    for (var i = -1; i <= 1; i++)
+                    {
+                        for (var j = -1; j <= 1; j++)
+                        {
+                            aliveNeighbors += currentGrid[row + i, column + j] == Status.Alive ? 1 : 0;
+                        }
+                    }
+                    var currentCell = currentGrid[row, column];
+
+                    // The cell needs to be subtracted from its neighbors as it was  
+                    // counted before 
+                    aliveNeighbors -= currentCell == Status.Alive ? 1 : 0;
+
+                    // Implementing the Rules of Life 
+
+                    // Cell is lonely and dies 
+                    if (currentCell == Status.Alive && aliveNeighbors < 2)
+                    {
+                        nextGeneration[row, column] = Status.Dead;
+                    }
+
+                    // Cell dies due to over population 
+                    else if (currentCell == Status.Alive && aliveNeighbors > 3)
+                    {
+                        nextGeneration[row, column] = Status.Dead;
+                    }
+
+                    // A new cell is born 
+                    else if (currentCell == Status.Dead && aliveNeighbors == 3)
+                    {
+                        nextGeneration[row, column] = Status.Alive;
+                    }
+                    // stays the same
+                    else
+                    {
+                        nextGeneration[row, column] = currentCell;
+                    }
+                }
+            return nextGeneration;
+        }
+
+        // Current_Grid() - Get the currrent status of the grid
+        //
+        // This will loop through the grid and determine which cell is alive or dead
+        //
+        public void Current_Grid()
+        {
+            for (int i = 0; i < number_of_Rows; i++)
+            {
+                for (int j = 0; j < number_of_Cols; j++)
+                {
+                    if (box[i, j].BackColor == Color.White)
+                        currentGen[i, j] = Status.Dead;
+                    else
+                        currentGen[i, j] = Status.Alive;
+                }
+            }
+        }
+
+        // Print_Grid() - Print the grid
+        //
+        // This will loop through the grid and set the color of every alive cell to Magenta, otherwise, to White
+        //
+        public void Print_Grid(Status[,] new_grid)
+        {
+            for (int i = 0; i < number_of_Rows; i++)
+            {
+                for (int j = 0; j < number_of_Cols; j++)
+                {
+                    if (new_grid[i, j] == Status.Dead)
+                        box[i, j].BackColor = Color.White;
+                    else
+                        box[i, j].BackColor = Color.Magenta;
+                }
+            }
+        }
+        // Reset_All_button_Click() - Call the Reset_Grid() method when this button is clicked.
+        //
+        // This will loop through the grid and reset every cell to dead status
+        //
+        private void Reset_All_button_Click(object sender, EventArgs e)
+        {
+            Reset_Grid();
+            label_grid_groupBox.Enabled = true;
+
+        }
+
+        // Reset_Grid() - Reset the grid, a helper method that will be triggered if reset button is clicked
+        // or when new grid is loaded.
+        //
+        // This will loop through the grid and reset every cell to dead status
+        //
+        private void Reset_Grid()
+        {
+            for (int row = 0; row < number_of_Rows; row++)
+            {
+                for (int col = 0; col < number_of_Cols; col++)
+                {
+                    box[row, col].BackColor = Color.White;
+
+                }
+            }
+        }
+
+        // Exit_button_Click() - Terminate Application
+        //
+        // This calls Application.Exit() to terminate the program
+        //
+        private void Exit_button_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+
+    }
+
+
+}
